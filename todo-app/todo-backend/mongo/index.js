e21bbcf987c3f8a5a71f8ff62ecb1934776c8113
ebaek88 <93978710+ebaek88@users.noreply.github.com>
2026-03-25 15:49:53 -0600

    feat: add answers for Part 12, up to Exercise 12.7

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,11 +1,19 @@
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
+const { MONGO_URL } = require("../util/config");
 
 if (MONGO_URL && !mongoose.connection.readyState) {
-  mongoose.connect(MONGO_URL)
+  console.log("Connecting to Mongo:", MONGO_URL);
+  mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((error) => {
+      console.error("MongoDB connection error:", error.message);
+    });
 }
 
 module.exports = {
-  Todo
-}
+  Todo,
+};
--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,11 @@
-const MONGO_URL = process.env.MONGO_URL || undefined
-const REDIS_URL = process.env.REDIS_URL || undefined
+require("dotenv").config();
+
+console.log("dotenv loaded MONGO_URL =", process.env.MONGO_URL);
+
+const MONGO_URL = process.env.MONGO_URL || undefined;
+const REDIS_URL = process.env.REDIS_URL || undefined;
 
 module.exports = {
   MONGO_URL,
-  REDIS_URL
-}
+  REDIS_URL,
+};
